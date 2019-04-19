7ca693917ce8221c560e0edc9ab281b415380f12
Marcelo Laos <mlaos1997@gmail.com>
2019-04-18 23:06:55 -0400

    update

---
+++ friends/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+
+    render() {
+        <div className="App">
+                <h1>Friends</h1>
+        </div>
+    }
+}
